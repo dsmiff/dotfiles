75d42ce589be6d8e127231a573548214983875ff
dsmiff <dominic.smith@sentiance.com>
2018-09-21 10:11:22 +0200

    Add useful git things

---
+++ git.sh
@@ -0,0 +1,4 @@
+alias gitlg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gp="git push origin HEAD"
+alias gs="git status"
+alias gb="git branch"
