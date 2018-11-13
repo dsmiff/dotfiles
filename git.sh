e9c94c57f484d971cb13f6f2d4a465cab906409a
dsmiff <dominic.smith@sentiance.com>
2018-11-13 16:29:44 +0100

    Add git autocomplete script for mac OS

--- git.sh
+++ git.sh
@@ -2,3 +2,6 @@ alias gitlg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Crese
 alias gpush="git push origin HEAD"
 alias gs="git status"
 alias gb="git branch"
+if [ -f ~/.git-completion.bash ]; then
+  . ~/.git-completion.bash
+fi
--- miscl.sh
+++ miscl.sh
@@ -1,5 +1,8 @@
+alias gp="grep -rI"
+alias gpy="grep -rI $1 --include=*.py"
+
 function pysparkNB() {
     export PYSPARK_DRIVER_PYTHON=jupyter
     export PYSPARK_DRIVER_PYTHON_OPTS='notebook'
-    pyspark
+    pyspark --driver-memory 12g --conf spark.driver.maxResultSize=10g
 }
